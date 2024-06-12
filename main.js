7eab5c76155db526ed29b79bb03fcfac058c7198
Juan Francisco Maldonado <juan.maldonado.leon@gmail.com>
2024-06-12 00:45:53 -0400

    funcion resta

---
+++ main.js
@@ -0,0 +1,20 @@
+/**
+ * Suma dos numeros y retorna el resultado
+ * @param {*} num1 
+ * @param {*} num2 
+ * @returns 
+ */
+function sumarNumeros(num1, num2) {
+    return num1 + num2;
+}
+
+
+/**
+ * 
+ * @param {*} num1 
+ * @param {*} num2 
+ * @returns 
+ */
+function restarNumeros(num1, num2) {
+    return num1 - num2;
+}
